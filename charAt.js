7db37735dda7fd5577ef6ab7bb46f0a478eb9aef
ariful4082 <ariful4082@gmail.com>
2021-11-07 00:39:02 +0600

    chatAt method is done

---
+++ charAt.js
@@ -0,0 +1,7 @@
+// The String object's charAt() method returns a new string consisting of the single UTF-16 code unit located at the specified offset into the string
+
+const sentence = "The quick brown fox jumps over the lazy dog.";
+
+const index = 4;
+
+console.log(`The character at index ${index} is ${sentence.charAt(index)}`);
