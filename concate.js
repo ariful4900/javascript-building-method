6442b703a38f0e4194d3668625b7c915ada22b51
ariful4082 <ariful4082@gmail.com>
2021-11-07 00:45:45 +0600

    concate method for string concatenations

---
+++ concate.js
@@ -0,0 +1,6 @@
+// The concat() method concatenates the string arguments to the calling string and returns a new string.
+
+const sentence = "Hello!  Abdullah al Zarif";
+const sentence2 = "How are you doing?";
+
+console.log(sentence.concat(", ", sentence2));
