4088cbeab95d33abf32428728fe614c046821b4a
ariful4082 <ariful4082@gmail.com>
2021-11-19 20:44:42 +0600

    modify the exapmplse of split method

--- split.js
+++ split.js
@@ -3,6 +3,8 @@ const str = "The quick brown fox jumps over the lazy dog";
 
 const words = str.split(" ");
 console.log(words[2]);
+console.log(words);
 
 const chars = str.split("");
 console.log(chars[5]);
+console.log(chars);
